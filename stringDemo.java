483c482115e1b1fc18ae07da7435352eb2924c01
ZoTag1for1 <zenzetew@gmail.com>
2024-07-25 14:19:56 +0700

    Create stringDemo.java

---
+++ stringDemo.java
@@ -0,0 +1,8 @@
+class stringDemo {
+    public static void main(String[] args) {
+        String greeting ;
+        greeting = "Good morning";
+        String name = " Computer";
+        System.err.println(greeting + name);
+    }
+}
