8fb1afd6b211f0a7de10ce6b7e43f16446828299
ZoTag1for1 <zenzetew@gmail.com>
2024-07-25 14:30:53 +0700

    Create exNum.java

---
+++ exNum.java
@@ -0,0 +1,8 @@
+class exNum {
+    public static void main(String[] args) {
+        int number1 = 62;
+        int number2 = 99;
+        int total = number1 + number2;
+        System.out.println("The sum of 62 and 99 is : " + total);
+    }
+}
