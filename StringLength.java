934ce390bcfa32aac162690e1fee717d96143023
ZoTag1for1 <zenzetew@gmail.com>
2024-07-26 10:51:01 +0700

    Create StringLength.java

---
+++ StringLength.java
@@ -0,0 +1,8 @@
+class StringLength {
+    public static void main(String[] args) {
+        String name = "Computer";
+        int stringSize;
+        stringSize = name.length();
+        System.err.println(name+" has " +stringSize+ " characters");
+    }
+}
