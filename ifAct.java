e18232350f5e083a4d115cb39ef822f8f037f93e
ZoTag1for1 <zenzetew@gmail.com>
2024-07-26 14:44:39 +0700

    Create ifAct.java

---
+++ ifAct.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ifAct {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your points: ");
+        int points = scanner.nextInt();
+
+        if (points > 60) {
+            System.out.println("You are pass");
+        } else {
+            System.out.println("You are not pass");
+        }
+        scanner.close();
+    }
+}
