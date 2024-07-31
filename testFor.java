b1ae826c5ec0aaed17a36f06084d488e9a816f1e
ZoTag1for1 <zenzetew@gmail.com>
2024-07-31 15:41:16 +0700

    Create testFor.java

---
+++ testFor.java
@@ -0,0 +1,8 @@
+public class testFor {
+    public static void main(String[] args) {
+        int number = 2;
+        for(int i = 1; i <= 12; i++){
+            System.out.println(number + " x " +i+ " = " + (number * i));
+        }
+    }
+}
