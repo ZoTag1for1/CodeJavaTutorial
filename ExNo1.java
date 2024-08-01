6ead88d3be669afa77ddea50854dc3973a90b772
ZoTag1for1 <zenzetew@gmail.com>
2024-08-01 13:41:02 +0700

    Create ExNo1.java

---
+++ ExNo1.java
@@ -0,0 +1,9 @@
+public class ExNo1 {
+    public static void main(String[] args) {
+        System.out.println("เลขคู่ตั้งแต่ 2 ถึง 100:");
+        
+        for (int i = 2; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
