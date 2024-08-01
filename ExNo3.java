d0714baa014b02106e4404cbb5f5f681bab7b40c
ZoTag1for1 <zenzetew@gmail.com>
2024-08-01 13:41:05 +0700

    Create ExNo3.java

---
+++ ExNo3.java
@@ -0,0 +1,10 @@
+public class ExNo3 {
+    public static void main(String[] args) {
+        int number = 8; 
+        System.out.println("สูตรคูณแม่ " + number + " :");
+        for (int i = 1; i <= 12; i++) { 
+            int result = number * i; 
+            System.out.println(number + " x " + i + " = " + result); 
+        }
+    }
+}
