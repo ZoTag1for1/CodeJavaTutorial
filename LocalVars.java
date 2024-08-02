dcda818950b956e5343bf0bd34e9ddeadc62c118
ZoTag1for1 <zenzetew@gmail.com>
2024-08-02 13:33:30 +0700

    Create LocalVars.java

---
+++ LocalVars.java
@@ -0,0 +1,15 @@
+public class LocalVars {
+    public static void main(String[] args) {
+       phuket();
+       rayoung();
+        
+    }
+    public static void phuket(){
+        int birds = 500;
+        System.out.println("In phuket there are " +birds+ " birds.");
+    }
+    public static void rayoung(){
+        int birds = 3500;
+        System.out.println("In rayoung there are " +birds+ " birds.");
+    }
+}
