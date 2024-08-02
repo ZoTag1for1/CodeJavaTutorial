c96064942a089320f678c390a49d186364ef9ba6
ZoTag1for1 <zenzetew@gmail.com>
2024-08-02 10:07:02 +0700

    Create DeepAadDeeper.java

---
+++ DeepAadDeeper.java
@@ -0,0 +1,15 @@
+public class DeepAadDeeper {
+    public static void main(String[] args) {
+        System.out.println("i am starting in main.");
+        deep();
+        System.out.println("Now i am backe in deep.");
+    }
+    public static void deep(){
+        System.out.println("i am now in deep.");
+        deeper();
+        System.out.println("Now i am backe in deep.");
+    }
+    public static void deeper(){
+        System.out.println("i am now in deeper.");
+    }
+}
