c82c8980e4ef11efecd29282796a2980a32a0b89
ZoTag1for1 <zenzetew@gmail.com>
2024-08-02 14:07:16 +0700

    Create BarChart.java

---
+++ BarChart.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.Scanner;
+public class BarChart {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);    
+        int x;
+        System.out.println("Input Data : ");
+        x = in.nextInt();
+        Bar(x);
+    }
+    public static void Bar(int data){
+        for(int x = 1; x<= data; x++){
+            System.out.println("*");
+        }
+        System.out.println();
+    }
+}
