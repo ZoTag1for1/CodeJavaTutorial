c05c8de788e5ed8d9df76ac155c23c6570678720
ZoTag1for1 <zenzetew@gmail.com>
2024-08-07 14:41:17 +0700

    Create LengthDemo.java

---
+++ LengthDemo.java
@@ -0,0 +1,13 @@
+public class LengthDemo {
+    public static void main(String[] args) {
+        Rectangle box = new Rectangle();
+        System.out.println("Sending the value 10.0 to the setLenhyj method");
+        box.setLength(10.0);
+        box.setWidth(20.0);
+        box.getLength();
+        System.out.println("The box's length is " +box.getLength());
+        System.out.println("The box's Width is " +box.getLength());
+        System.out.println("The box's Area is " +box.getArea());
+        System.out.println("Done");
+    }
+}
